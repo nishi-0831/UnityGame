dd849ee07b0aff62caefaf2d04f797590f01015f
西川和樹 <190053668+nishi-0831@users.noreply.github.com>
2025-09-30 22:53:20 +0900

    PlayerInteractableCreatorのテンプレートを修正

--- Assets/Editor/PlayerInteractableCreator.cs
+++ Assets/Editor/PlayerInteractableCreator.cs
@@ -222,7 +222,7 @@ public class {className} : PlayerInteractableBase
     {{
         // ‰¡Õ“Ë‚Ìˆ—‚ğ‚±‚±‚É‹Lq
         // —á:
-        // PlayerInteractionUtils.ApplyDamage(player, DamageToPlayer, Progress);
+        // PlayerInteractionUtils.ApplyDamage(player, DamageToPlayer);
     }}
 }}
 ";
