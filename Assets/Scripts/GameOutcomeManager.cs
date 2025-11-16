4e6a75b31f1a71b49326d2e3a30027430ab5595c
西川和樹 <190053668+nishi-0831@users.noreply.github.com>
2025-11-16 12:41:47 +0900

    GameOutcomeManagerからGameOverSE、GameClearSEを削除

--- Assets/Scripts/GameOutcomeManager.cs
+++ Assets/Scripts/GameOutcomeManager.cs
@@ -8,10 +8,7 @@ public class GameOutcomeManager : MonoBehaviour
     private event Action OnGameClear;
     private event Action OnGameOver;
     [SerializeField] private float transitionSceneDelay;
-    [SerializeField] private AudioClip gameOverSE;
-    [SerializeField][Range(0.0f,1.0f)] private float gameOverSEVolume = 0.5f;
-    [SerializeField] private AudioClip gameClearSE;
-    [SerializeField][Range(0.0f, 1.0f)] private float gameClearSEVolume = 0.5f;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
@@ -31,14 +28,12 @@ public class GameOutcomeManager : MonoBehaviour
     public void TriggerGameClear()
     {
         OnGameClear?.Invoke();
-        AudioManager.Instance.PlaySound(gameClearSE, gameClearSEVolume);
         TransitionScene.Instance.ToResult(transitionSceneDelay);
     }
 
     public void TriggerGameOver()
     {
         OnGameOver?.Invoke();
-        AudioManager.Instance.PlaySound(gameOverSE, gameOverSEVolume);
         TransitionScene.Instance.ToPause(transitionSceneDelay);
     }
     void Start()
