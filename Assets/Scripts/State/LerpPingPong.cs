92e6f2c1c2375782036787fe9d94514d5bd605c9
歩夢 坂本 <tara06130925@gmail.com>
2025-12-14 00:51:49 +0900

    プレスの予兆　今のところ点滅のみ

--- Assets/Scripts/State/LerpPingPong.cs
+++ Assets/Scripts/State/LerpPingPong.cs
@@ -1,9 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 public enum MoveState
 {
@@ -11,105 +7,86 @@ public enum MoveState
     GOING,
     COMBACKING
 }
+
 [RequireComponent(typeof(EaseInterpolator))]
+[RequireComponent(typeof(Rigidbody))]
 public class LerpPingPong : MonoBehaviour
 {
-    [Header("補間の始点")]
+    [Header("Position")]
     [SerializeField] public Vector3 _from;
-
-    [Header("線形補間の終点")]
     [SerializeField] public Vector3 _to;
-    
-    [Header("行きに掛かる時間")]
-    [SerializeField] private float goingTime = 1;
-    
-    [Header("帰りに掛かる時間")]
-    [SerializeField] private float comeBackTime = 3;
-    
-    [Header("行き始めるまでの時間")]
-    [SerializeField] private float againGoingTime = 2f;
 
-    [Header("戻り始めるまでの時間")]
-    [SerializeField] private float againComebackTime = 2f;
+    [Header("Time (seconds)")]
+    [SerializeField] private float goingTime = 2.0f;        // 押す時間
+    [SerializeField] private float comeBackTime = 0.6f;     // 戻る時間
+    [SerializeField] private float firstTime = 2.0f;        // 最初だけ待つ
+    public float FirstTime => firstTime;
 
-    [Header("最初、動き出すまでの時間")]
-    [SerializeField] private float firstTime = 0;
+    [SerializeField] private float againGoingTime = 1.5f;   // 次に押すまで
+    [SerializeField] private float againComebackTime = 0.0f;// 押し切ってから戻るまで
 
-    [SerializeField] private MoveState currentState_;
+    private MoveState currentState_;
+    public MoveState CurrentState => currentState_;
 
-    public MoveState CurrentState
-    {
-        get { return currentState_; }
-    }
-    public StateMachine<MoveState> stateMachine_;
-    
+    private StateMachine<MoveState> stateMachine_;
+    private EaseInterpolator interpolator;
     private Rigidbody rb;
-    
-    [SerializeField]EaseInterpolator interpolator;
+    private Coroutine waitCoroutine;
 
     private void Awake()
     {
-        rb = this.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         interpolator = GetComponent<EaseInterpolator>();
         stateMachine_ = new StateMachine<MoveState>();
+        InitializeStateMachine();
     }
-    private void Start()
-    {
-    }
+
     private void InitializeStateMachine()
     {
-
-        //WAIT状態の振る舞い
-        /*何もしない*/
+        // WAIT
         stateMachine_.RegisterState(MoveState.WAIT).SetCallbacks(
-            onEntry: () => 
+            onEntry: () =>
             {
-                // 前回の状態を参考にして一定時間待機、次の状態へ遷移
+                StopWaitCoroutine();
+
+                float waitTime = firstTime;
+                MoveState next = MoveState.GOING;
+
                 switch (stateMachine_.PrevState)
                 {
                     case MoveState.GOING:
-                        StartCoroutine(delay(againComebackTime, MoveState.COMBACKING));
-                        break;
-                    case MoveState.COMBACKING:
-                        StartCoroutine(delay(againGoingTime, MoveState.GOING));
-                        break;
-                    case MoveState.WAIT:
-                        StartCoroutine(delay(firstTime, MoveState.GOING));
-                        break;
-                    default:
-                        StartCoroutine(delay(firstTime, MoveState.GOING));
+                        waitTime = againComebackTime;
+                        next = MoveState.COMBACKING;
                         break;
 
+                    case MoveState.COMBACKING:
+                        waitTime = againGoingTime;
+                        next = MoveState.GOING;
+                        break;
                 }
-                
-            },
-            onUpdate: () => 
-            {
-            },
-            onExit: () => { }
-            );
-        
-       
-        // GOING状態の振る舞い
+
+                waitCoroutine = StartCoroutine(WaitAndTransition(waitTime, next));
+            }
+        );
+
+        // GOING（プレス）
         stateMachine_.RegisterState(MoveState.GOING).SetCallbacks(
             onEntry: () =>
             {
                 interpolator.duration = goingTime;
                 interpolator.isReverse_ = false;
+                interpolator.from_ = _from;
+                interpolator.to_ = _to;
                 interpolator.Reset();
             },
             onUpdate: () =>
             {
                 interpolator.UpdateTime();
                 Move();
-            },
-            onExit: () =>
-            {
-                
-            }).
-            AddTransition(MoveState.WAIT, ref interpolator.onFinished_);
+            }
+        ).AddTransition(MoveState.WAIT, ref interpolator.onFinished_);
 
-        // COMBACKING状態の振る舞い
+        // COMBACKING（戻り）
         stateMachine_.RegisterState(MoveState.COMBACKING).SetCallbacks(
             onEntry: () =>
             {
@@ -121,56 +98,48 @@ public class LerpPingPong : MonoBehaviour
             {
                 interpolator.UpdateTime();
                 Move();
-            },
-            onExit: () =>
-            {
-                
-            }).
-            AddTransition(MoveState.WAIT,ref interpolator.onComeback_);
+            }
+        ).AddTransition(MoveState.WAIT, ref interpolator.onComeback_);
     }
-    private async Task<bool> Wait(int delay)
-    {
-        await Task.Delay(delay * 1000).ConfigureAwait(false);
-        return true;
-    }
-    
-    
-    /// <summary>
-    /// 状態機を初期化し、始点と終点の座標を設定して往復移動を開始する
-    /// </summary>
+
     public void StartPingPong()
     {
-        InitializeStateMachine();
-        interpolator.onFinished_ += OnFinished;
-        interpolator.from_ = _from;
-        interpolator.to_ = _to;
-
+        StopWaitCoroutine();
         stateMachine_.Start(MoveState.WAIT);
     }
+
     private void Update()
     {
         stateMachine_.UpdateCurrent();
         currentState_ = stateMachine_.CurrentState;
     }
-    
 
-    private void OnFinished()
-    {
-        
-    }
-    
-     IEnumerator delay(float time,MoveState nextState)
-    {
-        yield return new WaitForSeconds(time);
-        stateMachine_.TransitionTo(nextState);
-    }
-   
-    /// <summary>
-    /// 始点から終点を補間した座標へ移動(Rigidbody.MovePosition を使用)
-    /// </summary>
     private void Move()
     {
-        Vector3 newPosition = interpolator.Interpolation();
-        rb.MovePosition(newPosition);
+        rb.MovePosition(interpolator.Interpolation());
+    }
+
+    private IEnumerator WaitAndTransition(float time, MoveState next)
+    {
+        if (time > 0f)
+            yield return new WaitForSeconds(time);
+
+        stateMachine_.TransitionTo(next);
+    }
+
+    private void StopWaitCoroutine()
+    {
+        if (waitCoroutine != null)
+        {
+            StopCoroutine(waitCoroutine);
+            waitCoroutine = null;
+        }
+    }
+
+    // 外部（PressMachine）から調整用
+    public void SetMoveTime(float going, float comeback)
+    {
+        goingTime = going;
+        comeBackTime = comeback;
     }
 }
